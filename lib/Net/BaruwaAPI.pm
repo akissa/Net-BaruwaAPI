a01087ead49ae4fe7f25e61c0ea93b45fdcafe40
Andrew Colin Kissa <andrew@topdog.za.net>
2015-10-15 07:44:51 +0200

    first commit

---
+++ lib/Net/BaruwaAPI.pm
@@ -0,0 +1,109 @@
+package Net::BaruwaAPI;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Net::BaruwaAPI - The great new Net::BaruwaAPI!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Net::BaruwaAPI;
+
+    my $foo = Net::BaruwaAPI->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Andrew Colin Kissa, C<< <andrew at topdog.za.net> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-net-baruwaapi at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Net-BaruwaAPI>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Net::BaruwaAPI
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Net-BaruwaAPI>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Net-BaruwaAPI>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Net-BaruwaAPI>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Net-BaruwaAPI/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2015 Andrew Colin Kissa.
+
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at L<http://mozilla.org/MPL/2.0/>.
+
+
+=cut
+
+1; # End of Net::BaruwaAPI
